d24e93be7992e5e7281eb2ac18549ecbce4d5224
ayodkay <ayodelekayode51@yahoo.com>
2019-10-15 01:39:23 -0300

    added ui

---
+++ app/src/main/java/com/ayodkay/app/fulllabdesfio/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.ayodkay.app.fulllabdesfio
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.cardview.widget.CardView
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
---
+++ app/src/main/java/com/ayodkay/app/fulllabdesfio/SubCategories.kt
@@ -0,0 +1,12 @@
+package com.ayodkay.app.fulllabdesfio
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SubCategories : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sub_categories)
+    }
+}
